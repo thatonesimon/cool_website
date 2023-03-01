af386c6c8da097c42ed99737e248769acb08ecd8
Simon Wong <swong@amazon.com>
2023-02-28 20:33:56 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a525865bea8a28c9165d93cbafd20c14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2bbd3710e5c5370e607",
+    "url": "/react-gh-pages/static/css/main.97004b25.chunk.css"
+  },
+  {
+    "revision": "a2bbd3710e5c5370e607",
+    "url": "/react-gh-pages/static/js/main.a2bbd371.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-gh-pages/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a09094aa04d3745b8b89f0d5c5803faa",
+    "url": "/react-gh-pages/index.html"
+  }
+];
