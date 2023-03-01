202aa745f90e9f3b9bc3d061c869094a27b58ec8
Simon Wong <swong@amazon.com>
2023-02-28 20:43:48 -0800

    Update homepage

---
+++ precache-manifest.fe4434bcc8f4f8bd1e522833ae608b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2bbd3710e5c5370e607",
+    "url": "/static/css/main.97004b25.chunk.css"
+  },
+  {
+    "revision": "a2bbd3710e5c5370e607",
+    "url": "/static/js/main.a2bbd371.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9507ba4b33c887128695df052bb47698",
+    "url": "/index.html"
+  }
+];
